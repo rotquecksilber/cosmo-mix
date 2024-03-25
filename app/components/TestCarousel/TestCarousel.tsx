341e9fee6d0fd29ad03755c41f9f7a98f83b69f6
Valentin <prefectzao@gmail.com>
2024-03-25 16:19:12 +0400

    EXP 27

--- app/components/TestCarousel/TestCarousel.tsx
+++ app/components/TestCarousel/TestCarousel.tsx
@@ -1,15 +1,36 @@
 import React from "react";
 import { motion } from "framer-motion";
-import Image from "next/image";
 import {ButtonBanner} from "@/app/components";
+import styles from './testCarousel.module.css'
+import cn from 'classnames'
 
-
-const CARD_COLORS = ["#266678", "#cb7c7a", "#36a18b"];
-const CARD_OFFSET = 40;
+const CARD_DATA = [
+    {
+        htagText: ["Кремы", "для лица"],
+        spanText: "Эффективный и роскошный уход",
+        imageAlt: "Контрактное производство кремов для лица",
+        imageSrc: "/home/creams_for_face_.webp"
+    },
+    {
+        htagText: ["Маски", "и патчи"],
+        spanText: "Интенсивный уход и отличное качество",
+        imageAlt: "Контрактное производство масок и патчей",
+        imageSrc: "/home/masks_.webp"
+    },
+    {
+        htagClassName: "button_htag_add",
+        spanClassName: "button_span_add",
+        htagText: ["Тоники"],
+        spanText: "Увлажняющий уход для кожи",
+        imageAlt: "Контрактное производство тоников для лица",
+        imageSrc: "/home/tonics_.webp"
+    },
+];
+const CARD_OFFSET = 30;
 const SCALE_FACTOR = 0.1;
 
 export const CardStack = () => {
-    const [cards, setCards] = React.useState(CARD_COLORS);
+    const [cards, setCards] = React.useState(CARD_DATA);
 
     const moveToEnd = (from: number) => {
         const newCards = [...cards];
@@ -19,19 +40,18 @@ export const CardStack = () => {
     };
 
     return (
-        <div style={wrapperStyle}>
-            <ul style={cardWrapStyle}>
-                {cards.map((color, index) => {
+        <div className={styles.wrapperStyle}>
+            <ul className={styles.cardWrapStyle}>
+                {cards.map((card, index) => {
                     const canDrag = index === 0;
 
                     return (
                         <motion.li
-                            key={color}
-                            style={{
-                                ...cardStyle,
-
-                                cursor: canDrag ? "grab" : "auto"
-                            }}
+                            key={card.imageSrc}
+                            className={cn(
+                                styles.cardStyle,
+                                canDrag ? styles._cursor_grab : styles._cursor_auto
+                            )}
                             animate={{
                                 x: index * CARD_OFFSET,
                                 scale: 1 - index * SCALE_FACTOR,
@@ -44,21 +64,17 @@ export const CardStack = () => {
                             }}
                             onDragEnd={() => moveToEnd(index)}
                             transition={{ type: "spring", stiffness: 300, damping: 30 }}
-
                         >
-                            <motion.div drag="x" dragConstraints={{left: 0, right: 0}}
-                                        onDragEnd={() => moveToEnd(index)}>
+                            <div>
                                 <ButtonBanner
-                                    htagClassName={"button_htag_add"}
-                                    spanClassName={"button_span_add"}
-                                    htagText={["Тоники"]}
-                                    spanText={"Увлажняющий уход для кожи"}
-                                    imageAlt={"Контрактное производство тоников для лица"}
-                                    imageSrc={"/home/tonics_.webp"}
-
+                                    htagText={card.htagText}
+                                    spanText={card.spanText}
+                                    imageAlt={card.imageAlt}
+                                    imageSrc={card.imageSrc}
+                                    htagClassName={card.htagClassName}
+                                    spanClassName={card.spanClassName}
                                 />
-                            </motion.div>
-
+                            </div>
                         </motion.li>
                     );
                 })}
@@ -72,7 +88,8 @@ const wrapperStyle: React.CSSProperties = {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    height: "100vh"
+    width: "100%", // увеличьте ширину
+    height: "450px"
 };
 
 const cardWrapStyle: React.CSSProperties = {
@@ -83,8 +100,10 @@ const cardWrapStyle: React.CSSProperties = {
 
 const cardStyle: React.CSSProperties = {
     position: "absolute",
-    width: "350px",
-    height: "220px",
+    top: 0,
+    left: 0,
+    width: "416px",
+    height: "450px",
     borderRadius: "8px",
     transformOrigin: "center center",
     listStyle: "none"
