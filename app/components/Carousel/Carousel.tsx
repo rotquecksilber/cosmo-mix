de7f8f2fd7b40b00184e94c422104dc71c29ec73
Valentin <prefectzao@gmail.com>
2024-04-14 12:10:53 +0400

    slick dots

--- app/components/Carousel/Carousel.tsx
+++ app/components/Carousel/Carousel.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode } from 'react';
 import Slider from 'react-slick';
 import './slick.css';
 import 'slick-carousel/slick/slick.css';
+import './slick-theme.css';
 import 'slick-carousel/slick/slick-theme.css';
 import styles from './Carousel.module.css';
 
